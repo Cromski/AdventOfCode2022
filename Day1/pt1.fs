1de6f2401db8bdd07ba26fc4fd533a7f4dd135a9
Cromski <90246462+Cromski@users.noreply.github.com>
2022-12-01 09:35:06 +0100

    day 1 pt 1

---
+++ Day1/pt1.fs
@@ -0,0 +1,14 @@
+let readLines filePath = List.ofSeq (System.IO.File.ReadLines(filePath))
+
+let wing arr =
+    let rec aux acc arr =
+        match arr with
+        | [] -> acc
+        | x :: xs -> 
+            match x with
+            | "" -> aux (0::acc) xs
+            | x -> aux ((acc.Head + (x |> int))::acc.Tail) xs
+
+    aux [0] arr
+
+printfn "%A" (List.max (wing (readLines "/Users/Cromski/Documents/Freetime/AdventOfCode2022/Day1/pt1-big.txt")))
