c0eb1e6b135cce3d9cc008471b3f5363d9602b42
Cromski <90246462+Cromski@users.noreply.github.com>
2022-12-01 09:54:18 +0100

    day 1, pt 2

---
+++ Day1/pt2.fs
@@ -0,0 +1,21 @@
+let readLines filePath = List.ofSeq (System.IO.File.ReadLines(filePath))
+
+let wing arr =
+    let rec aux acc arr =
+        match arr with
+        | [] -> acc
+        | x :: xs -> 
+            match x with
+            | "" -> aux (0::acc) xs
+            | x -> aux ((acc.Head + (x |> int))::acc.Tail) xs
+
+    aux [0] arr
+
+let rec addTop3 arr =
+    let rec aux arr acc iterCount = 
+        match arr with
+        | x :: xs when iterCount < 3 -> aux xs (acc+x) (iterCount+1)
+        | _ -> acc
+    aux arr 0 0
+
+printfn "%A" (addTop3 (List.sortDescending (wing (readLines "/Users/Cromski/Documents/Freetime/AdventOfCode2022/Day1/pt1-big.txt"))))
