e02542951dde4dd6d349a2b449f4a9f923680c48
Cromski <90246462+Cromski@users.noreply.github.com>
2022-12-04 13:37:52 +0100

    complete day 4, pt1 and pt2

---
+++ Day_04/pt2.fs
@@ -0,0 +1,23 @@
+open System
+let readLines filePath = List.ofSeq(IO.File.ReadLines(filePath))
+
+let splitLinesForEasierChecks (str: string): int list =
+    let strList = str.Split(",") |> Array.toList
+    let strList1 = strList[0].Split("-") |> Array.toList |> List.map int
+    let strList2 = strList[1].Split("-") |> Array.toList |> List.map int
+    strList1@strList2
+    
+let wing (arr: string list) =
+    let rec aux acc (arr: string list) =
+        match arr with
+        | [] -> acc
+        | x :: xs ->
+            let intList = splitLinesForEasierChecks x
+            if 
+                ((intList[0] < intList[2] && intList[0] < intList[3]) && (intList[1] < intList[2] && intList[1] < intList[3])) ||
+                ((intList[0] > intList[2] && intList[0] > intList[3]) && (intList[1] > intList[2] && intList[1] > intList[3])) // not overlapping
+            then aux acc xs
+            else aux (acc+1) xs
+    aux 0 arr
+
+printfn "%A" (wing (readLines "./Day_04/big.txt"))
