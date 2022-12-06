f37888aa520b57d6a7b9a521bf26454c78b07d3e
Cromski <90246462+Cromski@users.noreply.github.com>
2022-12-06 10:26:22 +0100

    complete day 6, pt1 & pt2

--- Day_06/pt1.fs
+++ Day_06/pt1.fs
@@ -1,5 +1,15 @@
 open System
-let readLines filePath = List.ofSeq(IO.File.ReadLines(filePath))
+let readLine filePath = List.ofSeq(IO.File.ReadAllText(filePath))
 
+let checkIfAnyCharsAreTheSame (a: char) b c d =
+    a <> b && a <> c && a <> d && b <> c && b <> d && c <> d
 
-printfn "%A" (wing (readLines "./Day_06/mini.txt"))
+let wing (cList: char list) =
+    let rec aux (acc: int) (cList: char list) = 
+        match cList with
+        | a :: b :: c :: d :: _ when checkIfAnyCharsAreTheSame a b c d -> acc+4
+        | _ :: b :: c :: d :: xs -> aux (acc+1) (b::c::d::xs)
+        | _ -> 0 // never happens
+    aux 0 cList
+
+printfn "%A" (wing (readLine "./Day_06/big.txt"))
--- Day_06/pt2.fs
+++ Day_06/pt2.fs
@@ -1,5 +1,21 @@
 open System
 let readLines filePath = List.ofSeq(IO.File.ReadLines(filePath))
 
+let rec trueIfAllCharsAreDifferent (str: string) =
+    let cList = str.ToCharArray() |> Array.toList
+    let rec aux (cList: char list) =
+        match cList with
+        | [] -> true
+        | x :: xs when String.Concat(Array.ofList(xs)).Contains x = true -> false
+        | x :: xs when String.Concat(Array.ofList(xs)).Contains x = false -> aux xs
+    aux cList
 
-printfn "%A" (wing (readLines "./Day_06/mini.txt"))
+let wing (strList: string list) =
+    let rec aux (acc: int) (strList: string list) = 
+        match strList with
+        | x :: _ when trueIfAllCharsAreDifferent (x.Substring(0,14)) -> acc+14
+        | x :: _ -> aux (acc+1) [(x.Substring(1,x.Length-1))]
+        | _ -> 0 // never happens
+    aux 0 strList
+
+printfn "%A" (wing (readLines "./Day_06/big.txt"))
